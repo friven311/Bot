ff39771f42b05f1ed3dcd1901148ecdf1d781dfe
friven311 <77752535+friven311@users.noreply.github.com>
2021-05-31 09:43:09 +0300

    First commit

---
+++ bot.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const telegraf = require(‘telegraf’);
+const api = require('covid19-api');
+console.log(Telegraf, Telegraf.Markup)
+
+const bot = new Telegraf(process.env.botToken);
+bot.start((ctx) => ctx.reply(`
+Привет, ${ctx.message.from.first_name}!
+Бот для того, чтобы узнать статистику о короне.
+Введи название страны латиницей.
+Посмотреть список стран можно командой /help.
+`));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.on('text', async (ctx) => {
+  let data = {};
+
+  try {
+  data = await api.getReportsByCountries(ctx.message.text);
+
+  const formatData = `
+Страна: ${data[0][0].country}
+Случаи: ${data[0][0].cases}
+Смертей: ${data[0][0].deaths}
+Вылечилось: ${data[0][0].recovered}
+  `;
+  ctx.reply(formatData);
+} catch {
+  console.log('error');
+  ctx.reply('Ошибка, такой страны нет');
+}
+});
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
+console.log('Бот запущен');
